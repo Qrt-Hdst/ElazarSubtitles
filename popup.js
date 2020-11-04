6015744607226d23a49954fefecd923156da128c
Hamilkar247 <adonnibal96@gmail.com>
2020-11-04 10:19:49 +0100

    nieskonczone

--- popup.js
+++ popup.js
@@ -1,11 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
-  document.querySelector('button').addEventListener('click',
-   onclick, false)
+  document.querySelector('button').addEventListener('click', onclick, false)
+
   function onclick() {
     chrome.tabs.query({currentWindow: true, active: true}, 
      function (tabs) {
-       chrome.tabs.sendMessage(tabs[0].id, 'hi')
+       chrome.tabs.sendMessage(tabs[0].id, 'hi', setCount)
      }
     )
+  }
+
+  function setCount (res) {
+    const div = document.createElement('div')
+    div.textContent = '${res.count} bears'
+    document.body.appendChild(div)
   }
 }, false)
